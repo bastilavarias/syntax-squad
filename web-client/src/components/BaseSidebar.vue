bb368443ef820e73cc7edb1635f0a08458b4fa22
topzdev <christianlugod05@gmail.com>
2024-05-28 21:28:21 +0800

    upd: appbar mobile responsive

---
+++ web-client/src/components/BaseSidebar.vue
@@ -0,0 +1,26 @@
+<template>
+    <Dialog>
+        <DialogTrigger>
+            <Button class="block lg:hidden" variant="outline">
+                <Menu />
+            </Button>
+        </DialogTrigger>
+        <DialogContent class="h-screen w-[300px] translate-x-0 left-0 !rounded-none">
+            <CustomSideBar />
+        </DialogContent>
+    </Dialog>
+</template>
+
+<script setup lang="ts">
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { ref } from "vue";
+import CustomSideBar from "@/components/CustomSideBar.vue";
+import { Menu } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+
+const drawer = ref(true);
+</script>
+
+<style scoped>
+
+</style>
--- web-client/src/layouts/MainLayout.vue
+++ web-client/src/layouts/MainLayout.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import BaseAppBar from "@/components/BaseAppBar.vue";
+import BaseSidebar from "@/components/BaseSidebar.vue";
 </script>
 
 <template>
-    <div class="relative">
+    <div class="relative pt-14">
         <BaseAppBar />
         <slot></slot>
     </div>
